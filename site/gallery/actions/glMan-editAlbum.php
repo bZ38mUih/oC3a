68941cabc73624278e29f815fe4a93879be2a30d
rj-of <rightjoint@yandex.ru>
2018-08-28 10:37:27 +0300

    gal-patch

--- site/gallery/actions/glMan-editAlbum.php
+++ site/gallery/actions/glMan-editAlbum.php
@@ -1,15 +1,14 @@
 <?php
+require_once ($_SERVER["DOCUMENT_ROOT"]."/source/accessorial_class.php");
 $Alb_rd = new recordDefault("galleryAlb_dt", "album_id");
 if(isset($_GET['alb_id']) and $_GET['alb_id']!=null){
     $Alb_rd->result['album_id'] = $_GET['alb_id'];
     $Alb_rd->copyOne();
-
     if(isset($_POST['albumName']) and $_POST['albumName']!=null){
         $Alb_rd->result['albumName']=htmlspecialchars($_POST['albumName']);
     }else{
         $albErr['albumName']='недопустимое название альбома';
     }
-
     if(isset($_POST['albumAlias']) and $_POST['albumAlias']!=null){
         if($Alb_rd->result['albumAlias']!=htmlspecialchars($_POST['albumAlias'])){
             $Alb_rd->result['albumAlias']=htmlspecialchars($_POST['albumAlias']);
@@ -20,16 +19,13 @@ if(isset($_GET['alb_id']) and $_GET['alb_id']!=null){
     }else{
         $albErr['albumAlias']='недопустимый alias - null';
     }
-
     if(isset($_POST['albumAlias']) and $_POST['albumAlias']!=null){
         $Alb_rd->result['albumAlias']=htmlspecialchars($_POST['albumAlias']);
     }else{
         $albErr['albumAlias']='недопустимый alias';
     }
     $Alb_rd->result['metaDescr']=htmlspecialchars($_POST['metaDescr']);
-
     if(isset($_POST['glCat_id'])){
-
         if($_POST['glCat_id'] == 'none'){
             $Alb_rd->result['glCat_id']=null;
         }else{
@@ -43,23 +39,17 @@ if(isset($_GET['alb_id']) and $_GET['alb_id']!=null){
     }else{
         $Alb_rd->result['activeFlag']=false;
     }
-
     if(isset($_POST['dateOfCr']) and $_POST['dateOfCr']!=null){
         $Alb_rd->result['dateOfCr']=$_POST['dateOfCr'];
     }
-
     if(isset($_POST['transAlbImg']) and $_POST['transAlbImg']!=null){
         $Alb_rd->result['transAlbImg']=$_POST['transAlbImg'];
     }else{
         $Alb_rd->result['transAlbImg']=null;
     }
-
-    //exit;
 }else{
     $albErr['album_id']='недопустимое alb_id';
 }
-
-
 if(isset($albErr)){
     $albErr['common']=false;
     require_once($_SERVER["DOCUMENT_ROOT"]."/site/gallery/views/glMan-editAlbum.php");
@@ -68,7 +58,6 @@ if(isset($albErr)){
         $albErr['common']=true;
         require_once($_SERVER["DOCUMENT_ROOT"]."/site/gallery/views/glMan-editAlbum.php");
     }else{
-//print_r($Alb_rd);
         $appRJ->response['result'].= "444<br>";
         $appRJ->response['result'].= "zhopa-edit";
     }
