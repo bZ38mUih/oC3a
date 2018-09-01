c9dee8396ed8851a8bbb7d62b3490bc69914bdd2
mrSmitch <rightjoint@yandex.ru>
2018-09-01 14:42:40 +0300

    likes-count-impr

--- source/shareCount.php
+++ source/shareCount.php
@@ -1,12 +1,13 @@
 <?php
-$fbResp = json_decode(file_get_contents("https://graph.facebook.com/?fields=og_object{likes.summary(total_count).limit(0)},share&id=http://"
+$fbResp = json_decode(file_get_contents("https://graph.facebook.com/?".
+    "fields=og_object{likes.summary(total_count).limit(0)},share&id=https://"
     .$_SERVER['HTTP_HOST'].$_SERVER['REDIRECT_URL']), true);
 $appRJ->response['result']['fb']=$fbResp['share']['share_count'];
 
-$okResp = json_decode(file_get_contents("https://connect.ok.ru/dk?st.cmd=extLike&tp=json&ref=http://"
+$okResp = json_decode(file_get_contents("https://connect.ok.ru/dk?st.cmd=extLike&tp=json&ref=https://"
     .$_SERVER['HTTP_HOST'].$_SERVER['REDIRECT_URL']), true);
 $appRJ->response['result']['ok']=$okResp['count'];
 
-$vkResp = file_get_contents("http://vk.com/share.php?act=count&url=http://".$_SERVER['HTTP_HOST'].$_SERVER['REDIRECT_URL']);
+$vkResp = file_get_contents("http://vk.com/share.php?act=count&url=https://".$_SERVER['HTTP_HOST'].$_SERVER['REDIRECT_URL']);
 $numSym = strpos($vkResp, ',');
 $appRJ->response['result']['vk'] = substr($vkResp, $numSym+1, strlen($vkResp)-2-$numSym-1);
