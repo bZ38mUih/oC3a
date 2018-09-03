e150ffb1f9fa00b66a8c305db4d922f2a447915b
rj-of <rightjoint@yandex.ru>
2018-09-03 17:20:51 +0300

    new-arts

--- site/artMan/actions/artMan-newArt_post.php
+++ site/artMan/actions/artMan-newArt_post.php
@@ -1,51 +1,40 @@
 <?php
-
 $artErr=null;
-
 $Art_rd = new recordDefault("art_dt", "art_id");
-
+require_once ($_SERVER["DOCUMENT_ROOT"]."/source/accessorial_class.php");
 if(isset($_POST['artName']) and $_POST['artName']!=null){
-    //$catName_err =
     $Art_rd->result['artName']=htmlspecialchars($_POST['artName']);
 }else{
     $artErr['artName']='недопустимое название статьи';
 }
-
 if(isset($_POST['artAlias']) and $_POST['artAlias']!=null){
     $Art_rd->result['artAlias']=htmlspecialchars($_POST['artAlias']);
     if(!accessorialClass::checkDouble("art_dt", "artAlias", $Art_rd->result['artAlias'])){
         $artErr['artAlias']='недопустимый alias - double ';
     }
-
 }else{
     $artErr['artAlias']='недопустимый alias - null';
 }
-
 if(isset($_POST['artMeta']) and $_POST['artMeta']!=null){
     $Art_rd->result['artMeta']=htmlspecialchars($_POST['artMeta']);
 }else{
     $Art_rd->result['artMeta']=null;
     $artErr['artMeta']='недопустимое описание';
 }
+if(isset($_POST['artCat_id'])) {
 
-if(isset($_POST['artCat_id'])){
-
-    if($_POST['glCat_id'] == 'none'){
-        $Art_rd->result['artCat_id']=null;
-    }else{
-        $Art_rd->result['artCat_id']=$_POST['artCat_id'];
+    if ($_POST['glCat_id'] == 'none') {
+        $Art_rd->result['artCat_id'] = null;
+    } else {
+        $Art_rd->result['artCat_id'] = $_POST['artCat_id'];
     }
-}else{
-    //$catErr['cat_id']='select';
 }
 if(isset($_POST['activeFlag']) and $_POST['activeFlag']=='on'){
     $Art_rd->result['activeFlag']=true;
 }else{
     $Art_rd->result['activeFlag']=false;
 }
-
 $Art_rd->result['pubDate'] = date_format($appRJ->date['curDate'], 'Y-m-d');
-
 if(isset($artErr)){
     require_once($_SERVER["DOCUMENT_ROOT"] . "/site/artMan/views/artMan-newArt.php");
 }else{
