e5a8cd2380b55c54d70ab06fac45e0930d7fd7b1
rj-of <rightjoint@yandex.ru>
2018-09-13 09:20:01 +0300

    scroll-top

--- site/js/goTop.js
+++ site/js/goTop.js
@@ -1,21 +1,29 @@
+var lastScrollTop = 0;
 $(document).ready(function() {
     $("body").append("<img src='/source/img/goTop.png' class='toTop' onclick='goTop()'>");
     $(document).scroll(function () {
-        if ($(this).scrollTop() > $(window).height()) {
-            if($("img.toTop").is(":visible")==false){
-                var timerInterval = setTimeout(function() {
-                    $("img.toTop").css("display", "none");
-                }, 3000);
-            }else {
-                clearInterval(timerInterval);
-            }
-            $("img.toTop").css("display", "block");
-        } else {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            // downscroll code
             $("img.toTop").css("display", "none");
+        } else {
+            // upscroll code
+            if ($(this).scrollTop() > $(window).height()) {
+                if($("img.toTop").is(":visible")==false){
+                    var timerInterval = setTimeout(function() {
+                        $("img.toTop").css("display", "none");
+                    }, 3000);
+                }else {
+                    clearInterval(timerInterval);
+                }
+                $("img.toTop").css("display", "block");
+            } else {
+                $("img.toTop").css("display", "none");
+            }
         }
+        lastScrollTop = st;
     });
 })
-
 function goTop() {
     $('html,body').animate({scrollTop: 0}, 1000);
 }
