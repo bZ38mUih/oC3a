7eb644a7ebd74e43f7957609da032fc31eeaaebc
rj-of <rightjoint@yandex.ru>
2018-11-06 15:03:52 +0300

    test-x2

--- site/handbook/actions/wpi.php
+++ site/handbook/actions/wpi.php
@@ -44,7 +44,7 @@ if (isset($_GET['wiSearch']) and $_GET['wiSearch']!=null){
         $vName=urldecode($appRJ->server['reqUri_expl'][3]);
         $vVal=urldecode($appRJ->server['reqUri_expl'][4]);
         //$wdInfo_qry="select * from wdEnvList_dt WHERE vName='".$vName."' and vVal='".$vVal."'";
-        $wdInfo_qry="select * from wdEnvList_dt WHERE vName='".$vName."' and vVal like '%".mysql_real_escape_string($vVal)."'%";
+        $wdInfo_qry="select * from wdEnvList_dt WHERE vName='".$vName."' and vVal like '%".mysql_real_escape_string($vVal)."%'";
         require_once ($_SERVER["DOCUMENT_ROOT"]."/site/handbook/views/wpi/wpi-env-print.php");
         $wdInfo.="</div>";
     }else{
