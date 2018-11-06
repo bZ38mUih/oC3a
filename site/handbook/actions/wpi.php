7a8b85b3fa9da1db1e53b601ef050c4d1c1c0ddc
rj-of <rightjoint@yandex.ru>
2018-11-06 14:25:48 +0300

    ltl-patch

--- site/handbook/actions/wpi.php
+++ site/handbook/actions/wpi.php
@@ -43,7 +43,7 @@ if (isset($_GET['wiSearch']) and $_GET['wiSearch']!=null){
         $wdInfo.="<div class='wi-block'>";
         $vName=urldecode($appRJ->server['reqUri_expl'][3]);
         $vVal=urldecode($appRJ->server['reqUri_expl'][4]);
-        $wdInfo_qry="select * from wdEnvList_dt WHERE vName='".$vName."' and vVal='".urldecode($vVal)."'";
+        $wdInfo_qry="select * from wdEnvList_dt WHERE vName='".$vName."' and vVal='".$vVal."'";
         require_once ($_SERVER["DOCUMENT_ROOT"]."/site/handbook/views/wpi/wpi-env-print.php");
         $wdInfo.="</div>";
     }else{
