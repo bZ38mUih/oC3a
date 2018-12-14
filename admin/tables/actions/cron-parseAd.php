58ee07bf0e4a6cf29c835ead78c527c2e2503e7f
rj-of <rightjoint@yandex.ru>
2018-12-14 12:41:37 +0300

    parse-ad-log

--- admin/tables/actions/cron-parseAd.php
+++ admin/tables/actions/cron-parseAd.php
@@ -11,6 +11,9 @@ $DB->connect_db();
 require_once ("/home/p264533/public_html/rightjoint.ru/source/recordDefault_class.php");
 //$pageCont = file_get_contents("https://www.avito.ru/ivanovo/telefony");
 //file_put_contents($_SERVER["DOCUMENT_ROOT"]."/temp/avito-test.html", $pageCont);
+//Array ( [noutbuki] => Array ( [err] => [totalCnt] => 47 [doubleCnt] => 0 [sussCnt] => 37 )
+// [planshety_i_elektronnye_knigi] => Array ( [err] => [totalCnt] => 0 [doubleCnt] => 0 [sussCnt] => 0 )
+// [telefony] => Array ( [err] => [totalCnt] => 0 [doubleCnt] => 0 [sussCnt] => 0 ) )
 $parseRes=null;
 $parseLog['noutbuki']['err']=null;
 $parseLog['noutbuki']['totalCnt']=0;
@@ -34,19 +37,23 @@ foreach ($parseLog as $key=>$value){
             $parseRD->result['adDate']=date_format($CurDate, "Y-m-d H:m:s");
             $parseRD->result['adType']=$key;
             if(!$posItem = strpos($pageCont, "item item_table ")){
+                $parseLog[$key]['err'].="нет posItem<br>";
                 break;
             }
             $parseLog[$key]['totalCnt']++;
             $pageCont=substr($pageCont, $posItem, strlen($pageCont));
             if(!$posRef1=strpos($pageCont, "class=\"title")){
+                $parseLog[$key]['err'].="нет posRef1<br>";
                 break;
             }
             $pageCont=substr($pageCont, $posRef1+17, strlen($pageCont));
             if(!$posRef2=strpos($pageCont, "href=\"")){
+                $parseLog[$key]['err'].="нет posRef2<br>";
                 break;
             }
             $pageCont=substr($pageCont, $posRef2+7, strlen($pageCont));
             if(!$posRef3=strpos($pageCont, "\"")){
+                $parseLog[$key]['err'].="нет posRef3<br>";
                 break;
             }
             $parseRD->result['prodRef']=urlencode(substr($pageCont, 0 , $posRef3));
@@ -55,28 +62,34 @@ foreach ($parseLog as $key=>$value){
                 break;
             }
             if(!$posProdName1=strpos($pageCont, "<span itemprop=\"name\">")){
+                $parseLog[$key]['err'].="нет posProdName1<br>";
                 break;
             }
             $pageCont=substr($pageCont, $posProdName1+22, strlen($pageCont));
             if(!$posProdName2=strpos($pageCont, "</span>")){
+                $parseLog[$key]['err'].="нет posProdName2<br>";
                 break;
             }
             $parseRD->result['prodName']=substr($pageCont, 0, $posProdName2);
             $pageCont=substr($pageCont, $posProdName2+7, strlen($pageCont));
             if(!$posPrice1=strpos($pageCont, "<span class=\"price\" itemprop=\"price\" content=\"")){
+                $parseLog[$key]['err'].="нет posPrice1<br>";
                 break;
             }
             $pageCont=substr($pageCont, $posPrice1+46, strlen($pageCont));
             if(!$posPrice2=strpos($pageCont, "\">")){
+                $parseLog[$key]['err'].="нет posPrice2<br>";
                 break;
             }
             $parseRD->result['prodPrice']=substr($pageCont, 0, $posPrice2);
             $pageCont=substr($pageCont, $posPrice2+2, strlen($pageCont));
             if(!$posComp1=strpos($pageCont, "<div class=\"data\">")){
+                $parseLog[$key]['err'].="нет posComp1<br>";
                 break;
             }
             $pageCont=substr($pageCont, $posComp1+18, strlen($pageCont));
             if(!$posComp2=strpos($pageCont, "<p>")){
+                $parseLog[$key]['err'].="нет posComp2<br>";
                 break;
             }
             if(!$prodComp=substr($pageCont, 0, $posComp2)){
@@ -88,31 +101,35 @@ foreach ($parseLog as $key=>$value){
             }
             //$descrCont = file_get_contents($_SERVER["DOCUMENT_ROOT"]."/temp/parse-ad/".$parseRD->result['prodRef'].".html");
             if(!$descrCont = file_get_contents("https://avito.ru/".urldecode($parseRD->result['prodRef']))){
-                $parseLog[$key]['err']="невозможно открыть описание";
+                $parseLog[$key]['err'].="невозможно открыть описание<br>";
                 break;
             };
             //file_put_contents($_SERVER["DOCUMENT_ROOT"]."/temp/parse-ad/".urlencode($parseRD->result['prodRef']).".html", $descrCont);
             if(!$posSaler1=strpos($descrCont, "seller-info-prop js-seller-info-prop_seller-name")){
+                $parseLog[$key]['err'].="нет posSaler1<br>";
                 break;
             }
             $dC=substr($descrCont, $posSaler1, strlen($descrCont));
             if(!$posSaler2=strpos($dC, "<div>")){
+                $parseLog[$key]['err'].="нет posSaler2<br>";
                 break;
             }
             $dC=substr($dC, $posSaler2+5, strlen($descrCont));
             if(!$posSaler3=strpos($dC, "</div>")){
+                $parseLog[$key]['err'].="нет posSaler3<br>";
                 break;
             }
             if(!$parseRD->result['prodSaler']=substr($dC, 0, $posSaler3)){
-                echo "no-saler<br>";
+                $parseLog[$key]['err'].="нет prodSaler<br>";
                 break;
             }
-
             if(!$posDescr1=strpos($descrCont, "class=\"item-description\"")){
+                $parseLog[$key]['err'].="нет posDescr1<br>";
                 break;
             }
             $descrCont=substr($descrCont, $posDescr1+25, strlen($descrCont));
             if(!$posDescr2=strpos($descrCont, "</div>")){
+                $parseLog[$key]['err'].="нет posDescr2<br>";
                 break;
             }
             $parseRD->result['prodDescr']=substr($descrCont, 0, $posDescr2);
@@ -131,10 +148,13 @@ foreach ($parseLog as $key=>$value){
                 "</div></div>";
             */
             if($parseLog[$key]['totalCnt']>100){
+                $parseLog[$key]['err'].="max totalCnt<br>";
                 break;
             }
             if($parseRD->putOne()){
                 $parseLog[$key]['sussCnt']++;
+            }else{
+                $parseLog[$key]['err'].="fail putOne<br>";
             }
         }
     }else{
