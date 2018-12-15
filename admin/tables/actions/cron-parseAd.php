5a5b345b8f7036a332e94fd5b4327e148db9e914
mrSmitch <rightjoint@yandex.ru>
2018-12-15 11:59:15 +0300

    pa-p

--- admin/tables/actions/cron-parseAd.php
+++ admin/tables/actions/cron-parseAd.php
@@ -3,16 +3,16 @@
 
 
 
-//require_once("/home/p264533/public_html/rightjoint.ru/source/DB_class.php");
-require_once($_SERVER["DOCUMENT_ROOT"]."/source/DB_class.php");
-//require_once ("/home/p264533/public_html/rightjoint.ru/source/accessorial_class.php");
-require_once ($_SERVER["DOCUMENT_ROOT"]."/source/accessorial_class.php");
+require_once("/home/p264533/public_html/rightjoint.ru/source/DB_class.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/source/DB_class.php");
+require_once ("/home/p264533/public_html/rightjoint.ru/source/accessorial_class.php");
+//require_once ($_SERVER["DOCUMENT_ROOT"]."/source/accessorial_class.php");
 $DB=new DB();
-//$DB->connSettings=json_decode(@file_get_contents("/home/p264533/public_html/rightjoint.ru".$DB->pathToConn), true);
-$DB->connSettings=json_decode(@file_get_contents($_SERVER["DOCUMENT_ROOT"].$DB->pathToConn), true);
+$DB->connSettings=json_decode(@file_get_contents("/home/p264533/public_html/rightjoint.ru".$DB->pathToConn), true);
+//$DB->connSettings=json_decode(@file_get_contents($_SERVER["DOCUMENT_ROOT"].$DB->pathToConn), true);
 $DB->connect_db();
-//require_once ("/home/p264533/public_html/rightjoint.ru/source/recordDefault_class.php");
-require_once ($_SERVER["DOCUMENT_ROOT"]."/source/recordDefault_class.php");
+require_once ("/home/p264533/public_html/rightjoint.ru/source/recordDefault_class.php");
+//require_once ($_SERVER["DOCUMENT_ROOT"]."/source/recordDefault_class.php");
 $parseRes=null;
 $parseLog['noutbuki']['err']=null;
 $parseLog['noutbuki']['totalCnt']=0;
