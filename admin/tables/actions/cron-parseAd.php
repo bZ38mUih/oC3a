db400075b967be37c4dd3860dcaaec70670870c7
mrSmitch <rightjoint@yandex.ru>
2018-12-17 20:42:14 +0300

    fp-3

--- admin/tables/actions/cron-parseAd.php
+++ admin/tables/actions/cron-parseAd.php
@@ -156,7 +156,7 @@ foreach ($parseLog as $key=>$value){
 }
 //print_r($parseLog);
 $insertLog_qry="insert into parseAdLog_dt (logDate, logContent) ".
-    "VALUES ('".date_format($CurDate, "Y-m-d H:m:s")."', '". mysql_real_escape_string(json_encode($parseLog, true))."')";
+    "VALUES ('".date_format($CurDate, "Y-m-d H:i:s")."', '". mysql_real_escape_string(json_encode($parseLog, true))."')";
 $DB->doQuery($insertLog_qry);
 //echo $insertLog_qry;
 exit;
