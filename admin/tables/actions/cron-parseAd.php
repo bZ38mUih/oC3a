c0b4086286b0d86b6636577573262556aeb5d626
rj-of <rightjoint@yandex.ru>
2018-12-17 09:58:32 +0300

    pa-debug

--- admin/tables/actions/cron-parseAd.php
+++ admin/tables/actions/cron-parseAd.php
@@ -150,6 +150,8 @@ foreach ($parseLog as $key=>$value){
         $parseLog[$key]['err'].="невозможно открыть страницу<br>";
     }
 }
+echo "<h1>ParseLog:</h1>";
+print_r($parseLog);
 $insertLog_qry="insert into parseAdLog_dt (logDate, logContent) VALUES ('".date_format($CurDate, "Y-m-d H:m:s")."', '". json_encode($parseLog, true)."')";
 $DB->doQuery($insertLog_qry);
 exit;
