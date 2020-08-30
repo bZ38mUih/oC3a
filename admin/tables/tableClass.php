58d7081ad06bd61131253995e298cecc0d455863
bZ38mUih <alenanice37@gmail.com>
2020-08-30 17:20:51 +0300

    admin-not-used

--- admin/tables/tableClass.php
+++ admin/tables/tableClass.php
@@ -9,8 +9,6 @@ class tablesClass
         foreach (glob($_SERVER["DOCUMENT_ROOT"].DB_UPLOADS."tablesList/*_dt.php") as $filename){
             $this->tables[substr(basename($filename),0, strlen(basename($filename))-4)]['list']=true;
         }
-        //echo "<pre>";
-        //print_r( $this->tables);
     }
 
     public function dbCompare($DB)
@@ -20,20 +18,10 @@ class tablesClass
         $query_res = $DB->query($query_text);
         while ($query_row = $query_res->fetch(PDO::FETCH_ASSOC)) {
             foreach ($this->tables as $table => $value) {
-                //echo $query_row['TABLE_NAME'] ."-xxx-". $table."<br>";
-
-                if(lower_case_table_names == 2){
-                    if ( strtolower($query_row['TABLE_NAME']) == strtolower($table)) {
-                        $this->tables[$table]['exist'] = true;
-                        break;
-                    }
-                }else{
-                    if ( $query_row['TABLE_NAME'] == $table) {
-                        $this->tables[$table]['exist'] = true;
-                        break;
-                    }
+                if ( $query_row['TABLE_NAME'] == $table) {
+                    $this->tables[$table]['exist'] = true;
+                    break;
                 }
-
             }
             if (!isset($this->tables[$table])) {
                 $this->tables[$table]['list'] = false;
